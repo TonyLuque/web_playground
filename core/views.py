992228e3e293d8f4ee7f8426edc3b7835e7a9693
Anthony Luque <kasuru@hotmail.com>
2020-11-09 16:05:42 -0500

    [ADD] core app, static files and templates files

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+
+    template_name = 'core/home.html'
+
+
+class SamplePageView(TemplateView):
+
+    template_name = 'core/sample.html'
