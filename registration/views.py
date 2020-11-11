d8696baa6525d1f51cffa5f3df09554b0a07bf47
Anthony Luque <kasuru@hotmail.com>
2020-11-11 12:05:59 -0500

    [ADD] email field on register's form

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
@@ -6,7 +6,7 @@ from django import forms
 # Create your views here.
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     # success_url = reverse_lazy('login') # No se puede concatenar, entoces se redefine el metodo get_success_url
     template_name = "registration/signup.html"
 
@@ -17,6 +17,7 @@ class SignUpView(CreateView):
         form = super(SignUpView, self).get_form()
         # Modificar en tiempo real
         form.fields['username'].widget = forms.TextInput(attrs={'class':'name_class', 'placeholder':'Nombre de usuario'})
+        form.fields['email'].widget = forms.EmailInput(attrs={'class':'name_class', 'placeholder':'email@dominio.com'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'placeholder':'Contraseña'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'placeholder':'Repita la contraseña'})
         return  form
