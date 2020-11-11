1c8143d0891f5c6ca7bd5278cfe840491b4cc77c
Anthony Luque <kasuru@hotmail.com>
2020-11-11 11:42:56 -0500

    [ADD] improve signup's form

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django import forms
 
 # Create your views here.
 
@@ -11,3 +12,11 @@ class SignUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register'
+
+    def get_form(self, form_class=None):
+        form = super(SignUpView, self).get_form()
+        # Modificar en tiempo real
+        form.fields['username'].widget = forms.TextInput(attrs={'class':'name_class', 'placeholder':'Nombre de usuario'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'placeholder':'Contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'placeholder':'Repita la contraseña'})
+        return  form
