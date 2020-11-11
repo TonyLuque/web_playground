802f0f1777ac1935e0d1d74a9324a757bd2683c3
Anthony Luque <kasuru@hotmail.com>
2020-11-10 20:36:01 -0500

    [ADD] mixins to protec endpoints

--- pages/views.py
+++ pages/views.py
@@ -3,22 +3,33 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import redirect
 from .forms import PageForm
 
 # Create your views here.
+
+class StaffRequiredMixin(object):
+    """
+        Este mixin requerira que el usuario sea miembro del staff
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
 class PagesListView(ListView):
     model = Page
 
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     # fields = ['title', 'content', 'order'] # Se borra ya que esta en la clase PageForm
     success_url = reverse_lazy('pages:pages')
 
-class PageUpdate(UpdateView):
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
@@ -26,6 +37,6 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
-class PageDelete(DeleteView):
+class PageDelete(StaffRequiredMixin,DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
