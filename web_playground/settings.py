435a8e896c57e6eb5261dd8ad214cc38d1b12000
Anthony Luque <kasuru@hotmail.com>
2020-11-11 10:58:08 -0500

    [ADD] logout function

--- web_playground/settings.py
+++ web_playground/settings.py
@@ -132,4 +132,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Auth redirects
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'pages:pages'
+LOGOUT_REDIRECT_URL = 'home'
