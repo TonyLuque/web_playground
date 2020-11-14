675df1189ccb83a2c9836e13dfd90cc83f23e5ed
Anthony Luque <kasuru@hotmail.com>
2020-11-14 12:01:43 -0500

    [ADD] messenger's views and templates

--- web_playground/urls.py
+++ web_playground/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 from django.conf import settings
 
 urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('profiles/', include(profiles_patterns)),
+    path('messenger/', include(messenger_patterns)),
     path('admin/', admin.site.urls),
     # Paths auth
     path('accounts/', include('django.contrib.auth.urls')),
